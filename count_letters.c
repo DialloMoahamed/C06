4a3a780dc85726ab8ec333e3e98bc3e3d03f7813
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-30 04:37:40 +0100

    Commit

--- count_letters.c
+++ count_letters.c
@@ -6,13 +6,12 @@ int main() {
     char chaine[100];
     int voyelles = 0, consonnes = 0;
 
-    printf("Entrez une chaîne : ");
     fgets(chaine, sizeof(chaine), stdin);
 
     for (int i = 0; chaine[i] != '\0'; i++) {
         char caractere = tolower(chaine[i]); 
 
-        if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u') {
+        if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u' || caractere == 'y') {
             voyelles++;
         } else if (isalpha(caractere)) {
             consonnes++;
