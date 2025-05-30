f7c30dfe5153d02b2ef9e5282a2e60df3c372dc6
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-30 02:13:46 +0100

    Commit

---
+++ count_letters.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    char chaine[150];
+    int Voyelles=0,Consonnes=0; 
+
+    fgets(chaine, sizeof(chaine), stdin);
+
+    for (int i = 0; chaine[i] != '\0'; i++)
+    {
+        char chaineEnVoyelle = tolower(chaine[i]);
+        if (chaineEnVoyelle == 'a' || chaineEnVoyelle == 'e' || chaineEnVoyelle == 'i' || chaineEnVoyelle == 'o' || chaineEnVoyelle == 'u' || chaineEnVoyelle == 'y')
+        {
+            Voyelles++;
+        }
+        else if (chaineEnVoyelle >= 'a' && chaineEnVoyelle <= 'z')
+        {
+            Consonnes++;
+        }
+        
+    }
+
+    printf("Voyelles : %d\nConsonnes : %d",Voyelles, Consonnes);
+    
+    return 0;
+}
