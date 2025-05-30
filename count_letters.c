ac973a384c97c8750c402c30fb25fd9a234dbeea
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-30 15:12:29 +0100

    Commit

--- count_letters.c
+++ count_letters.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
-#include <ctype.h>
 
 int main() {
     char str[1000];
-    int voyelles = 0, consonnes = 0;
+    int i = 0;
+    int voyelles = 0;
+    int consonnes = 0;
     char c;
 
-    fgets(str, sizeof(str), stdin);
+    // lit toute la ligne jusqu'au retour à la ligne
+    scanf("%[^\n]", str); 
 
-    for (int i = 0; str[i] != '\0'; i++) {
-        c = tolower(str[i]);
+    while (str[i] != '\0') {
+        c = str[i];
 
+        // Convertir manuellement en minuscule si besoin
+        if (c >= 'A' && c <= 'Z') {
+            c = c + 32;
+        }
+
+        // Vérifie si c'est une lettre entre 'a' et 'z'
         if (c >= 'a' && c <= 'z') {
+            // Vérifie si c'est une voyelle
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y') {
                 voyelles++;
             } else {
                 consonnes++;
             }
         }
+
+        i++;
     }
 
     printf("Voyelles : %d\n", voyelles);
